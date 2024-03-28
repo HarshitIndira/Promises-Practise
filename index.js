5b7a0c0e7633e7a8f8941af9e189ae1dd76ba741
HarshitIndira <sharshit@indiratrade.com>
2024-03-28 16:26:57 +0530

    New program created

--- index.js
+++ index.js
@@ -50,30 +50,65 @@
 //     console.log(error);
 // });
 
-console.log("Code execution started...")
-const APIResult = new Promise((resolve, reject)=>{
+// console.log("Code execution started...")
+// const APIResult = new Promise((resolve, reject)=>{
     
-    const random = Math.random()
+//     const random = Math.random()
 
-    if(random>0.8){
-        resolve("Data fetched successfullyy...")
-    }else{
-        reject("API failed to fetch the data...");
-    }
+//     if(random>0.8){
+//         resolve("Data fetched successfullyy...")
+//     }else{
+//         reject("API failed to fetch the data...");
+//     }
+// });
+
+// for(let i=0;i<5;i++){
+    
+//     APIResult.then((data)=>{
+//         console.log(data);
+//     }).catch((error)=>{
+//         console.log(error);
+//     });
+    
+// }
+
+
+
+
+const table = new Promise((resolve, reject)=>{
+    
+    setTimeout(() => {
+        const tableOfThree = [];
+        for (let i = 1; i <= 10; i++) {
+            tableOfThree.push(`3 * ${i} = ${3 * i}`);
+        }
+        resolve(tableOfThree);
+    }, 1000); 
+    
+
+    // setTimeout(()=>{
+    //     for(let i=1;i<=10;i++){
+    //         resolve();
+    //     }
+       
+    // },2000);
 });
 
-for(let i=0;i<5;i++){
-    
-    APIResult.then((data)=>{
-        console.log(data);
-    }).catch((error)=>{
-        console.log(error);
+
+
+for(let i=1;i<=10;i++){
+    console.log(`2 * ${i} = ${2*i}`);
+} 
+
+
+
+table.then((data)=>{
+    data.forEach((entry) => {
+        console.log(entry);
     });
-    
-}
-
-
-
+}).catch((error)=>{
+    console.log(error);
+})
 
 
 
